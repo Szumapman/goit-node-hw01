93e44975267686bbc32146e0518b263e0c154544
Paweł Szumański <pawel.szumanski@outlook.com>
2025-03-28 16:55:19 +0100

    add listContacts

--- contacts.js
+++ contacts.js
@@ -2,4 +2,12 @@ const fs = require('fs').promises;
 const path = require('path');
 
 const contactsPath = path.join(__dirname, 'db', 'contacts.json');
-    
+
+const listContacts = async () => {
+    try {
+        const data = await fs.readFile(contactsPath);
+        return JSON.parse(data);
+    } catch (error) {
+        error.log(error);
+    }
+};
